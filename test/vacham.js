d9a02619a5e956a90e44c80544880b88427a9fe0
Hung <Emailnguyenthanhhung@gmail.com>
2024-03-08 15:57:09 +0700

    update 03/08/2024

--- test/vacham.js
+++ test/vacham.js
@@ -1,45 +1,46 @@
 var myCanvas = document.getElementById("myCanvas");
-var context =myCanvas.getContext("2d");
+var context = myCanvas.getContext("2d");
 
-nhanvat={
-    x:100, y:500
+var nhanvat = {
+    x: 100,
+    y: 500
 }
 
-setInterval(start,100);
+setInterval(start, 100);
 
-
-function start(){
-    context.clearRect(0,0,300,600)
-    context.fillStyle="red";
-    context.fillRect(nhanvat.x,nhanvat.y,25,25);
+function start() {
+    context.clearRect(0, 0, 300, 600);
+    context.fillStyle = "red";
+    context.fillRect(nhanvat.x, nhanvat.y, 25, 25);
     xulyvacham();
 }
-function xulyvacham(){
-    if(nhanvat.x <=0|| nhanvat.x +25 >=300){
-        console.log("Bạn đã thua")
+
+function xulyvacham() {
+    if (nhanvat.x <= 0 || nhanvat.x + 25 >= 300 || nhanvat.y <= 0 || nhanvat.y + 25 >= 600) {
+        gameOver();
     }
-    // else{(nhanvat.y<=0|| nhanvat.y +25 >=600)
-    //     console.log("Bạn đã thua")
-    // }
 }
-document.addEventListener("keydown", key =>{
-     switch(key.keyCode){
-        case 37 :
-            if(nhanvat.x >0)
-            nhanvat.x -=5;
+
+function gameOver() {
+    console.log("Bạn đã thua");
+}
+
+document.addEventListener("keydown", key => {
+    switch (key.keyCode) {
+        case 37:
+            if (nhanvat.x > 0)
+                nhanvat.x -= 5;
             break;
         case 38:
-            nhanvat.y -=5;
+            nhanvat.y -= 5;
             break;
         case 39:
-            if(nhanvat.x + 25 <300)
-            nhanvat.x +=5;
+            if (nhanvat.x + 25 < 300)
+                nhanvat.x += 5;
             break;
-        case 40 :
-            nhanvat.y +=5;
-            break;    
+        case 40:
+            nhanvat.y += 5;
+            break;
 
-     }
+    }
 });
-
- 
